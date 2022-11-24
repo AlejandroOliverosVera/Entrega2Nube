38f99d61f87631f0cc1bc73c9e76d5a977ee1b63
Alejandro Oliveros <alejandro.oliveros@alumnos.uv.cl>
2022-11-23 22:40:41 -0300

    checkpoint2

--- wait-for.sh
+++ wait-for.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-
 # original script: https://github.com/eficode/wait-for/blob/master/wait-for
-
-
 
 TIMEOUT=15
 QUIET=0
-
 
 echoerr() {
   if [ "$QUIET" -ne 1 ]; then printf "%s\n" "$*" 1>&2; fi
@@ -17,52 +13,49 @@ usage() {
   exitcode="$1"
   cat << USAGE >&2
 Usage:
-  $cmdname host:port|url [-t timeout] [-- command args]
+  $cmdname host:port [-t timeout] [-- command args]
   -q | --quiet                        Do not output any status messages
   -t TIMEOUT | --timeout=timeout      Timeout in seconds, zero for no timeout
-                                      Defaults to 15 seconds
--- COMMAND ARGS                     Execute command with args after the test finishes
+  -- COMMAND ARGS                     Execute command with args after the test finishes
 USAGE
   exit "$exitcode"
 }
 
 wait_for() {
- for i in 'seq $TIMEOUT' ; do
-  nc -z "$HOST" "$PORT" > /dev/null 2>&1
-  result=$?
+  for i in seq $TIMEOUT ; do
+    nc -z "$HOST" "$PORT" > /dev/null 2>&1
 
-   if [ $result -eq 0 ] ; then
-     if [ $# -gt 0 ] ; then
-      exec "$@"
-     fi
-     exit 0
-   fi
-  sleep 1
- done
- echo "Operation timed out" >&2
- exit 1
+    result=$?
+    if [ $result -eq 0 ] ; then
+      if [ $# -gt 0 ] ; then
+        exec "$@"
+      fi
+      exit 0
+    fi
+   sleep 1
+  done
+  echo "Operation timed out" >&2
+  exit 1
 }
 
-while [$# -gt 0]
-
+while [ $# -gt 0 ]
 do
-
- case "$1" in
- *:* )
+  case "$1" in
+    : )
     HOST=$(printf "%s\n" "$1"| cut -d : -f 1)
     PORT=$(printf "%s\n" "$1"| cut -d : -f 2)
     shift 1
     ;;
- -q | --quiet)
+    -q | --quiet)
     QUIET=1
     shift 1
     ;;
- -t)
+    -t)
     TIMEOUT="$2"
-    if["$TIMEOUT"=""]; then break; fi
+    if [ "$TIMEOUT" = "" ]; then break; fi
     shift 2
     ;;
-    -timeout=*)
+    --timeout=*)
     TIMEOUT="${1#*=}"
     shift 1
     ;;
@@ -80,11 +73,9 @@ do
   esac
 done
 
-
-if [ "$HOST" = "" ] || [ "$PORT" = "" ]; then
-      echoerr "Error: you need to provide a host and port to test."
-      usage 2
+if [ "$HOST" = "" -o "$PORT" = "" ]; then
+  echoerr "Error: you need to provide a host and port to test."
+  usage 2
 fi
-
 
 wait_for "$@"
